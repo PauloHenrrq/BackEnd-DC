23af9f6acff52c2442796fd4009defe059e17be0
unknown <paulorodrigues0855@digitalcollege.internal>
2025-01-04 12:01:30 -0300

    Inicialização em Back-End com Express

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-import express from 'express' // IMPORTAÇÃO DA BIBLIOTECA EXPRESS   
+import express from 'express' // IMPORTAÇÃO DA BIBLIOTECA EXPRESS
 
 const app = express() // DECLARAÇÃO DO EXPRESS PARA UTILIZAR COMO APP
 const port = 3000 // DECLARAÇÃO DA PORTA COMO 3000
 
-app.get("/", (request, response) => response.json("Leo"))
+app.post('/', (request, response) => {
+    return response.json('Leo')
+  })
 
-app.listen(port, (error) => {
+app.get('/', (request, response) => {
+  return response.json('Leo')
+})
+
+app.listen(port, error => {
   console.log('App is running') // EXECUÇÃO DO CONTEÚDO UTILIZANDO LISTEN E DECLARAÇÃO DA PORTA, POR FIM CHAMANDO UMA CALLBACK
 })
