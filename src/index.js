528e7dc15d34dc1e02836cf1887f1590198b6769
unknown <paulorodrigues0855@digitalcollege.internal>
2025-03-15 11:41:54 -0300

    implementação do JWT e Bcrypt | Criação da pasta middleware | Falta: Fazer com que as rotas se tornem seguras

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import jwt from 'jsonwebtoken'
 
 import productRoutes from './routes/product.js'
 import userRoutes from './routes/user.js'
@@ -11,4 +12,29 @@ app.use(express.json())
 app.use(productRoutes)
 app.use(userRoutes)
 
-export default app;
+const secretKey =
+  'wa1f651sf94awfaw8f1aw81#4281$1f8wfa18wfasf2@$@3Fwafa5sf1w23edszshkb'
+
+app.get('/sign/:data', (req, res) => {
+  const verify = req.params.data
+
+  try {
+    if (verify === 'paulohenrique@gmail.com') {
+      const jwtToken = jwt.sign(
+        {
+          data: 'paulohenrique@gmail.com'
+        },
+        secretKey,
+        { expiresIn: '1h' }
+      )
+
+      return res.status(200).json(jwtToken)
+    } else {
+        return res.status(403).json({ message: 'Unauthorized' })
+    }
+  } catch (error) {
+    return res.status(400).json({ message: `Erro: ${error.message}` })
+  }
+})
+
+export default app
---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken'
+
+const secretKey =
+  'wa1f651sf94awfaw8f1aw81#4281$1f8wfa18wfasf2@$@3Fwafa5sf1w23edszshkb'
+
+const authMiddleware = (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+      return res.status(403).json({ message: 'Unauthorized' })
+    }
+
+    const token = authHeader.split(' ')[1]
+    if (!token) {
+      return res.status(403).json({ message: 'Unauthorized' })
+    }
+
+    jwt.verify(token, secretKey)
+    next()
+  } catch (error) {
+    return res.status(401).json(`Unauthorized. Error: ${error.message}`)
+  }
+}
+
+export default authMiddleware
