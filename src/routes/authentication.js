c2f0e4ec4f1cd02d607fb557d9e198d6dfef5621
PH <paulofera159@gmail.com>
2025-03-17 18:06:09 -0300

    autentication -> authentication

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,6 +1,6 @@
 import e from "express";
 
-import controllerGetAuthentication from "../controller/controllerAutentication.js";
+import controllerGetAuthentication from "../controller/controllerAuthentication.js";
 const authenticationRoute = e.Router()
 
 authenticationRoute.get('/sign/:data', controllerGetAuthentication)
